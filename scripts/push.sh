ade3320dcf3fae5278232c27ea11728d29f4d988
Ethan Sharabi <ethans@wix.com>
2018-03-29 16:14:52 +0300

    try to setup git permissions

---
+++ scripts/push.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email ${GIT_EMAIL}
+  git config --global user.name ${GIT_USERNAME}
+}
+
+bump_version() {
+  npm run bump:patch
+  git push 
+}
+
+setup_git
+bump_version
